dbecc6e4864fd3d0a112e9707eb45b249aaa5468
Spenser Reinhardt <sreinhardt@nagios.com>
2014-03-03 11:34:26 -0600

    plugins/check_swap.c - Copyright changes

--- plugins/check_swap.c
+++ plugins/check_swap.c
@@ -4,7 +4,7 @@
 * 
 * License: GPL
 * Copyright (c) 2000 Karl DeBisschop (kdebisschop@users.sourceforge.net)
-* Copyright (c) 2000-2007 Nagios Plugins Development Team
+* Copyright (c) 2000-2014 Nagios Plugins Development Team
 * 
 * Description:
 * 
@@ -28,7 +28,7 @@
 *****************************************************************************/
 
 const char *progname = "check_swap";
-const char *copyright = "2000-2007";
+const char *copyright = "2000-2014";
 const char *email = "devel@nagios-plugins.org";
 
 #include "common.h"
