848a90d5ce9b69e077d7de7b9d7867563afc51fa
John C. Frickson <jfrickson@nagios.com>
2016-07-20 16:41:16 -0500

    [BUG] check_dhcp broken on BSD
    Fix for issue https://github.com/nagios-plugins/nagios-plugins/issues/70

--- plugins-root/check_dhcp.c
+++ plugins-root/check_dhcp.c
@@ -326,7 +326,8 @@ int get_hardware_address(int sock,char *interface_name){
 #elif defined(__bsd__)
 						/* King 2004	see ACKNOWLEDGEMENTS */
 
-        int                     mib[6], len;
+        int                     mib[6];
+        size_t                  len;
         char                    *buf;
         unsigned char           *ptr;
         struct if_msghdr        *ifm;
