a61eed670148cfd753c60001285a9fee751e430e
Andreas Piesk <pieska@users.noreply.github.com>
2017-10-18 21:22:05 +0200

    don't issue http requests when checking cert only

--- plugins/check_http.c
+++ plugins/check_http.c
@@ -1025,12 +1025,10 @@ check_http (void)
     microsec_ssl = deltime (tv_temp);
     elapsed_time_ssl = (double)microsec_ssl / 1.0e6;
     if (check_cert == TRUE) {
-			result = np_net_ssl_check_cert(days_till_exp_warn, days_till_exp_crit);
-			if (result != STATE_OK) {
-				np_net_ssl_cleanup();
-				if (sd) close(sd);
-				return result;
-			}
+      result = np_net_ssl_check_cert(days_till_exp_warn, days_till_exp_crit);
+      np_net_ssl_cleanup();
+      if (sd) close(sd);
+      return result;
     }
   }
 #endif /* HAVE_SSL */
