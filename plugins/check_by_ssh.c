8af8b37c3154f9302974f28a355e7942a6d88902
madlohe <swolf@nagios.com>
2021-12-02 17:03:58 -0600

    check_by_ssh: fix documentation - -w/-c do not exist for this plugin

--- plugins/check_by_ssh.c
+++ plugins/check_by_ssh.c
@@ -430,7 +430,6 @@ print_help (void)
   printf ("    %s\n", _("Tell ssh to use this configfile [optional]"));
   printf (" %s\n","-q, --quiet");
   printf ("    %s\n", _("Tell ssh to suppress warning and diagnostic messages [optional]"));
-	printf (UT_WARN_CRIT);
 	printf (UT_CONN_TIMEOUT, DEFAULT_SOCKET_TIMEOUT);
 	printf (UT_VERBOSE);
 	printf("\n");
