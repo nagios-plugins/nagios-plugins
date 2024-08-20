87a9d49daf43db9071e3bfd439e6602a4f88e055
Marcel Telka <marcel@telka.sk>
2024-08-20 21:57:05 +0200

    Fix check_http segfault when SSL connection fails

--- plugins/check_http.c
+++ plugins/check_http.c
@@ -1080,9 +1080,9 @@ check_http (void)
     if (use_ssl == TRUE) {
         gettimeofday (&tv_temp, NULL);
         result = np_net_ssl_init_with_hostname_version_and_cert(sd, (use_sni ? host_name : NULL), ssl_version, client_cert, client_privkey);
-        if (verbose) printf ("SSL initialized\n");
         if (result != STATE_OK)
-            die (STATE_CRITICAL, NULL);
+            exit (STATE_CRITICAL);
+        if (verbose) printf ("SSL initialized\n");
         microsec_ssl = deltime (tv_temp);
         elapsed_time_ssl = (double)microsec_ssl / 1.0e6;
         if (check_cert == TRUE) {
